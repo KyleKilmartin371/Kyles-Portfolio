0e9c97b77b6abdaab90189444af63d57b14569c2
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-11-07 13:36:31 -0800

    nav and about bootstrap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import profileImage from '../../assets/profile-image.jpg';
+import React from "react";
+import profileImage from "../../assets/profile-image.jpg";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Row, Col } from 'react-bootstrap';
 
 function About() {
-    return (
-<section className="my-5">
-  <h1 id="about">About Kyle Kilmartin</h1>
-  <img src={profileImage} className="my-2" style={{ width: "25%" }} alt="profile" />
-</section>
-      );
+  return (
+    <Container>
+      <Row>
+          <Col xs={12} md={6}>
+            <h1 id="about">About Kyle Kilmartin</h1>
+            </ Col>
+            </Row>
+            <Row>
+          <Col xs={12} md={6}>
+            <p>
+              I am a young and energetic worker that is able to work both on my
+              own and well with others.
+              <br />
+              As a truly self-motivated individual, I have performed well
+              working with others in group settings as well as taking charge as
+              a leader and accomplishing tasks on my own.
+              <br />I can describe myself as an outgoing, social individual that
+              loves to communicate with others and build relationships that will
+              increase workflow.
+            </p>
+          </Col>
+          <Col>
+            <img
+              src={profileImage}
+              style={{ width: "70%" }}
+              alt="profile"
+            />
+          </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,35 +1,27 @@
 import React from "react";
-
+import { Container, Row, Col } from "react-bootstrap";
 
 function Nav() {
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <a data-testid="link" href="/">
-          Kyle Kilmartin
-        </a>
-      </h1>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#about" >
-              About me
-            </a>
-          </li>
-          <li className="mx-2">
-            <a href="#portfolio" >
-              Portfolio
-            </a>
-          </li>
-          <li className="mx-2">
-            <a href="#contact" >
-              Contact
-            </a>
-          </li>
-          <li className="mx-2"></li>
-        </ul>
-      </nav>
-    </header>
+    <Container>
+      <Row>
+        <Col xs={12} md={6}>
+          <h1>
+            <a href="/"> Kyle Kilmartin </a>
+          </h1>
+        </Col>
+
+        <Col xs={12} md={2}>
+          <a href="#about">About me</a>
+        </Col>
+        <Col xs={12} md={2}>
+          <a href="#portfolio">Portfolio</a>
+        </Col>
+        <Col xs={12} md={2}>
+          <a href="#contact">Contact</a>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
