c5b9376c869bfd8b025f3f0fe88d9f5728b4c880
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-11-07 21:06:11 -0800

    readme

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,37 +1,48 @@
 import React from "react";
 import profileImage from "../../assets/profile-image.jpg";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import {Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
   return (
-    <Container id='about' className='about'>
+    <Container id="about" className="about">
       <Row>
-          <Col xs={12} md={6}>
-            <h1 id="about">About Kyle Kilmartin</h1>
-            </ Col>
-            </Row>
-            <Row>
-          <Col xs={12} md={6}>
-            <p>
-              I am a young and energetic worker that is able to work both on my
-              own and well with others.
-              <br />
-              As a truly self-motivated individual, I have performed well
-              working with others in group settings as well as taking charge as
-              a leader and accomplishing tasks on my own.
-              <br />I can describe myself as an outgoing, social individual that
-              loves to communicate with others and build relationships that will
-              increase workflow.
-            </p>
-          </Col>
-          <Col>
-            <img
-              src={profileImage}
-              style={{ width: "70%" }}
-              alt="profile"
-            />
-          </Col>
+        <Col xs={12} md={6}>
+          <h1 id="about">About Kyle Kilmartin</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <p>
+            I am a young and energetic worker that is able to work both on my
+            own and well with others.
+            <br />
+            As a truly self-motivated individual, I have performed well working
+            with others in group settings as well as taking charge as a leader
+            and accomplishing tasks on my own.
+            <br />I can describe myself as an outgoing, social individual that
+            loves to communicate with others and build relationships that will
+            increase workflow.
+          </p>
+          <br />
+          <h4>Skills</h4>
+          <ul>
+            <li>JavaScript</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>JSON</li>
+            <li>RESTful API's</li>
+            <li>Handlebars</li>
+            <li>Sequelize</li>
+            <li>MySQL database</li>
+            <li>MongoDB</li>
+            <li>React</li>
+            <li>Jest testing</li>
+            <li>Git / GitHub</li>
+          </ul>
+        </Col>
+        <Col>
+          <img src={profileImage} style={{ width: "70%" }} alt="profile" />
+        </Col>
       </Row>
     </Container>
   );
