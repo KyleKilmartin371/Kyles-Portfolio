fb3e505ae8d4f7244c228b36357cafc1c618c0c2
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-11-29 18:57:30 -0800

    Routers on App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-      <main>
-      <About></About>
-      <Portfolio></Portfolio>
-      <Contact></Contact>
-      </main>
-      <Footer></Footer>
-    </div>
+    <Router>
+      {/* <div> */}
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<About />} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/Contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+      {/* </div> */}
+    </Router>
   );
 }
 
