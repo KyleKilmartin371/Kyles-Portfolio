ad593c62b34908e178d37073d4eb5839ebf99a5f
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-12-08 17:24:48 -0800

    updated navbar grid spacing for screen sizes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,23 +6,23 @@ function Nav() {
   return (
     <Container className="navbar">
       <Row className="navbar-row">
-        <Col xs={12} md={6}>
+        <Col xs={12} md={12} lg ={6}>
           <h1>
             <a href="/"> Kyle Kilmartin </a>
           </h1>
         </Col>
 
-        <Col xs={12} md={2}>
+        <Col xs={12} md={4} lg={2}>
             <h2>
           <Link to="/About">About me</Link>
           </h2>
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={4} lg={3}>
             <h2>
           <Link to="/portfolio">Portfolio</Link>
           </h2>
         </Col>
-        <Col xs={12} md={1}>
+        <Col xs={12} md={4} lg={1}>
             <h2>
           <Link to="/contact">Contact</Link>
           </h2>
